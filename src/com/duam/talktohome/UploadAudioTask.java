ddb5a66e37423989cb016798246f23cc28447537
Luis Pablo <luispablo.gallo@gmail.com>
2014-02-18 23:34:18 -0300

    Recibir la respuesta al mensaje.

--- src/com/duam/talktohome/UploadAudioTask.java
+++ src/com/duam/talktohome/UploadAudioTask.java
@@ -3,19 +3,27 @@ package com.duam.talktohome;
 import static com.duam.talktohome.ConstantesTalkToHome.SERVER_HOST;
 import static com.duam.talktohome.ConstantesTalkToHome.SERVER_PORT;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketTimeoutException;
 
+import android.media.MediaPlayer;
 import android.os.AsyncTask;
+import android.os.Environment;
 import android.util.Log;
 
 public class UploadAudioTask extends AsyncTask<String, Void, Exception>
 {
 	private static final String TAG = UploadAudioTask.class.getName();
+
+	private static final int PACKET_SIZE = 1024;
+	private static final String OK_MESSAGE = "OK";
+	private static final String AUX_FILE_NAME = Environment.getExternalStorageDirectory().getAbsolutePath() +"/aux_audio_file.3gp";
 
 	@Override
 	protected Exception doInBackground(String... params)
@@ -52,6 +60,9 @@ public class UploadAudioTask extends AsyncTask<String, Void, Exception>
 			Log.d(TAG, "File content sent");
 			
 			fis.close();
+			
+			receiveResponse(clientSocket);
+			
 			clientSocket.close();
 		}
 		catch (SocketTimeoutException ex)
@@ -65,4 +76,64 @@ public class UploadAudioTask extends AsyncTask<String, Void, Exception>
 
 		return null;
 	}
+	
+	private void receiveResponse(DatagramSocket socket)
+	{
+		Log.d(TAG, "Starting to receive response...");
+		try
+		{
+			byte[] bytes = new byte[PACKET_SIZE];
+			DatagramPacket receivePacket = new DatagramPacket(bytes, bytes.length);
+			
+			socket.setSoTimeout(40000);
+			Log.d(TAG, "About to receive file size...");
+			socket.receive(receivePacket);
+			Log.d(TAG, "received!");
+			long fileSize = Long.parseLong(new String(receivePacket.getData()).trim());
+			
+			BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(AUX_FILE_NAME));
+			
+			byte[] response = OK_MESSAGE.getBytes(); 
+			
+			int count = 0;
+			
+			while (count < fileSize)
+			{				
+				bytes = new byte[(fileSize > PACKET_SIZE) ? PACKET_SIZE : (int) fileSize];
+				DatagramPacket packet = new DatagramPacket(bytes, bytes.length);
+				Log.d(TAG, "About to receive chunk...");
+				socket.receive(packet);
+				bos.write(bytes);
+		
+				InetAddress IPAddress = packet.getAddress();
+				int port = packet.getPort();
+				
+				Log.d(TAG, "Sending response...");
+				DatagramPacket responsePacket = new DatagramPacket(response, response.length, IPAddress, port);
+				socket.send(responsePacket);
+				
+				count += bytes.length;
+				long remaining = fileSize - count;
+
+				if (remaining > 0)
+				{
+					bytes = new byte[(remaining < PACKET_SIZE) ? (int) remaining : PACKET_SIZE];
+				}
+			}
+			bos.flush();
+			bos.close();
+			Log.d(TAG, "finished!");
+			
+			Log.d(TAG, "About to play file");
+			MediaPlayer mp = new MediaPlayer();
+            mp.setDataSource(AUX_FILE_NAME);
+            mp.prepare();
+            mp.start();			
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}				
+	}
+	
 }
